d4aa62dd8fe98f4be9cad7f7668ef007212f92d7
shahhuzaifa <143290124+shahhuzaifa@users.noreply.github.com>
2023-09-08 17:00:06 +0500

    Add files via upload

---
+++ page.js.js
@@ -0,0 +1,103 @@
+//first name
+var firstName = "shah";
+alert(firstName);
+//last name
+var lastName = "huzaifa ";
+alert(lastName);
+// email
+var email = "shahhuzaifa@email.com";
+alert(email);
+//phone numder 
+var phoneNumber = " phoneNumber= 123-5657-78910";
+alert(phoneNumber);
+//password 
+var password = "password =123456";
+alert(password );
+//meassage 
+var meassage="message= Hello WE Are Learning Javascript ";
+alert(meassage);
+
+                       //chapter2 
+
+
+var myVariable;
+
+
+myVariable = "guru ";
+
+console.log(myVariable );
+
+
+//team name 
+
+var teamName = " TEAM 1 ";
+alert(teamName);
+
+
+    //This statement has already been coded
+    
+    var bestMan = "Charlie"; 
+    bestMan = "David"; 
+
+    console.log (bestMan);
+
+                                   // Chapter 3 (Variables for numbe
+  var caseQty 
+  caseQty=144;
+  console.log(caseQty);
+
+  var num = 9;
+  var result = num + 1;
+  console.log(  result);
+  
+  var sum = 5 + 7;
+  console.log("Sum: " + sum);
+
+
+  var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+var myNumber = 10;
+myNumber = myNumber + 5; 
+console.log( myNumber);
+
+var productCost = 3.45;
+console.log( productCost);
+
+var Nameofband ;
+
+var myNumber = 42;
+console.log( myNumber);
+
+var shahHuzaifa = "ShahHuzaifa";
+console.log( shahHuzaifa);
+
+
+var remainder = 10 % 3;
+console.log( remainder);
+
+var num = 20 % 6;
+console.log( num );
+
+var largeNum = 1000 * 2000; 
+console.log( largeNum);
+
+var a = 10; 
+var b = 5;  
+
+var result = a-b; 
+console.log( result);
+
+var c = 20; 
+var g = 6;   
+
+var remainder = c % g;
+
+var shah1 = 10;
+var shah2 = 2; 
+
+var result = shah1*shah2; 
+
+console.log(result);
